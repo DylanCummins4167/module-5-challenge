6dec6ae4b301f3a92d098a467ad8c3090b87c862
DylanCummins4167 <129929151+DylanCummins4167@users.noreply.github.com>
2023-05-17 03:25:12 -0500

    Update script.js

--- script.js
+++ script.js
@@ -38,33 +38,7 @@ $(function () {
 // Update the clock every second
 setInterval(updateClock, 1000);
   //
-  const rows = document.getElementsByClassName("row");
-let currentHour = parseInt(moment().format('H'));
-
-Array.from(rows).forEach(row => {
-  let
-    rowIdString = row.id,
-    rowHour;
-  if (rowIdString) {
-    rowHour = parseInt(rowIdString);
-  }
-  if (rowHour) {
-    // Compares row id to current hour and sets color accordingly
-    if (currentHour === rowHour) {
-      setColor(row, "red");
-    } else if ((currentHour < rowHour) && (currentHour > rowHour - 6)) {
-      setColor(row, "green");
-    } else if ((currentHour > rowHour) && (currentHour < rowHour + 6)) {
-      setColor(row, "lightgrey");
-    } else {
-      setColor(row, "white");
-    }
-  }
-});
-
-function setColor(element, color) {
-  element.style.backgroundColor = color;
-}
+  
   
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
